2da00bf0e84477ae4b948f91ca0d6f74fb2a9ec9
landlight <ck.light@gmail.com>
2022-04-16 18:52:33 +0700

    :recycle: format

--- src/jumpSearch.ts
+++ src/jumpSearch.ts
@@ -4,23 +4,21 @@ function jumpSearch(inputArray, searchItem)
   let step = Math.sqrt(n);
   
   let prev = 0;
-  while (inputArray[Math.min(step, n)-1] < searchItem)
-  {
-      prev = step;
-      step += Math.sqrt(n);
-      if (prev >= n)
-          return -1;
+  while (inputArray[Math.min(step, n)-1] < searchItem) {
+    prev = step;
+    step += Math.sqrt(n);
+    if (prev >= n)
+      return -1;
   }
   
-  while (inputArray[prev] < searchItem)
-  {
+  while (inputArray[prev] < searchItem) {
     prev++;
-
     if (prev == Math.min(step, n))
-        return -1;
+      return -1;
   }
+  
   if (inputArray[prev] == searchItem)
-      return prev;
+    return prev;
   
   return -1;
 }
